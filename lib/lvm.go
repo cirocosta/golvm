90d2e787596b92429c922c51e9fd8bd53fb8e52e
Ciro S. Costa <ciro.costa@liferay.com>
2017-11-14 22:09:25 -0200

    adds pretty bytes; updates command creation

--- lib/lvm.go
+++ lib/lvm.go
@@ -170,19 +170,119 @@ func (l Lvm) LogicalVolumes() (output []byte, err error) {
 	return
 }
 
-type LogicalVolumeCreationConfig struct {
-	IsSnapshot bool
-	Thinpool   string
-	IsSnapshot bool
-	Keyfile    string
-	// Size represents the size of the logical volume.
-	// It can be represented as a string in the format
-	// of '1G', '25.3M', etc
-	Size uint64
+type LvCreationConfig struct {
+	Name        string
+	Size        string
+	Snapshot    string
+	KeyFile     string
+	ThinPool    string
+	VolumeGroup string
 }
 
-// Creates a logical volume using the definition passed.
-func (l Lvm) CreateLogicalVolume(def *LogicalVolume) (err error) {
+// CreateLogicalVolume creates a logical volume using the definition passed.
+// A VolumeGroup my be specified or not.
+// notes.:
+//	-	a snapshot is a volume that is created from
+//		another volume that must already exist.
+func (l Lvm) BuildLogicalVolumeCretionArgs(cfg *LvCreationConfig) (args []string, err error) {
+	l.logger.Debug().
+		Str("name", cfg.Name).
+		Str("size", cfg.Size).
+		Str("snapshot", cfg.Snapshot).
+		Str("thinpool", cfg.ThinPool).
+		Str("keyfile", cfg.KeyFile).
+		Str("volume-group", cfg.VolumeGroup).
+		Msg("starting logical volume creation args building")
+
+	var (
+		isThinSnapshot = false // TODO detect this
+		isSnapshot     = cfg.Snapshot != ""
+		hasSize        = cfg.Size != ""
+		hasKeyFile     = cfg.KeyFile != ""
+		hasThinPool    = cfg.ThinPool != ""
+		finfo os.FileInfo
+	)
+
+	if cfg.Name == "" {
+		err = errors.Errorf("Name must be set")
+		return
+	}
+
+	if cfg.VolumeGroup == "" {
+		err = errors.Errorf("VolumeGroup must be specified")
+	}
+
+	if hasKeyFile {
+		finfo, err = os.Stat(cfg.KeyFile)
+		if err != nil {
+			err = errors.Wrapf(err,
+				"failed to inspect keyfile %s",
+				cfg.KeyFile)
+			return
+		}
+
+		if finfo.IsDir() {
+			err = errors.Errorf(
+				"keyfile %s must be a file, not a dir",
+				cfg.KeyFile)
+			return
+		}
+
+		_, err = exec.LookPath("cryptsetup")
+		if err != nil {
+			err = errors.Wrapf(err,
+				"cryptsetup not found in PATH")
+			return
+		}
+	}
+
+	if isSnapshot {
+		if hasKeyFile {
+			err = errors.Errorf("can't have snapshot with keyfile")
+			return
+		}
+
+		// TODO:check if the volumegroup is thinly provisioned
+		//	if is ==> mark as thinsnap
+	}
+
+	if !hasSize && !isThinSnapshot {
+		err = errors.Errorf("a size must be provided")
+		return
+	}
+
+	if hasSize && isThinSnapshot {
+		err = errors.Errorf("can't specify size for thin snapshots")
+		return
+	}
+
+
+	args = []string{"--setactivationskip", "n"}
+	args = append(args, "--name", cfg.Name)
+
+	switch {
+	case isSnapshot:
+		args = append(args, "--snapshot")
+
+		if hasSize {
+			args = append(args, "--size", cfg.Size)
+		}
+
+		args = append(args, cfg.VolumeGroup+"/"+cfg.Snapshot)
+	case hasThinPool:
+		args = append(args, "--virtualsize", cfg.Size)
+		args = append(args, "--thin")
+		args = append(args, cfg.VolumeGroup+"/"+cfg.ThinPool)
+	default:
+		args = append(args, "--size", cfg.Size)
+		args = append(args, cfg.VolumeGroup)
+	}
+
+	return
+}
+
+// DeleteLogicalVolume deletes a logical volume if it exists
+func (l Lvm) DeleteLogicalVolume(def *LogicalVolume) (err error) {
 	return
 }
 
@@ -211,3 +311,4 @@ func (l Lvm) run(name string, args ...string) (out []byte, err error) {
 
 	return
 }
+
---
+++ lib/prettybytes.go
@@ -0,0 +1,36 @@
+package lib
+
+import (
+	"github.com/pkg/errors"
+
+	units "github.com/docker/go-units"
+)
+
+func MustFromHumanSize(size string) uint64 {
+	bytes, err := units.FromHumanSize(size)
+	if err != nil {
+		err = errors.Wrapf(err,
+			"Couldn't convert string in human size (size=%s) to bytes",
+			size)
+		panic(err)
+	}
+
+	return uint64(bytes)
+}
+
+func FromHumanSize(size string) (bytes uint64, err error) {
+	bytesInt, err := units.FromHumanSize(size)
+	if err != nil {
+		err = errors.Wrapf(err,
+			"Couldn't convert string in human size (size=%s) to bytes",
+			size)
+		return
+	}
+
+	bytes = uint64(bytesInt)
+	return
+}
+
+func HumanSize(size uint64) string {
+	return units.HumanSize(float64(size))
+}
